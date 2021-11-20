4748c5aecc8ec704d7b96d991a5ac9e12d622beb
Alejandro Caicedo <alejandrocaicedo21216@gmail.com>
2021-11-20 02:19:02 -0500

    Proyecto Crud-2

--- backend/controllers/proyecto.controllers.js
+++ backend/controllers/proyecto.controllers.js
@@ -6,12 +6,39 @@ const crearProyecto = async (req, res) => {
     const newProyecto = new ProyectoModel(proyecto);
     await newProyecto.save();
     console.log("Pryecto creado con exito", newProyecto);
-    res.json(newProyecto)
+    res.json(newProyecto);
   } catch (error) {
-      console.error('Error Creado proyecto',error)
+    console.error("Error Creado proyecto", error);
+  }
+};
+
+const getProyectos = async (req, res) => {
+  try {
+    const proyectos = await ProyectoModel.find().populate("lider");
+    console.log("Obteniendo Proyectos");
+    res.json(proyectos);
+  } catch (error) {
+    console.error("Error al consultar proyectos", error);
+  }
+};
+
+const updateProyecto = async (req, res) => {
+  const id = req.params.id;
+  const { objetivosGenerales } = req.body;
+  try {
+    const updateProyecto = await ProyectoModel.findByIdAndUpdate(
+      id,
+      { objetivosGenerales },
+      { new: true }
+    );
+    res.json(updateProyecto)
+  } catch (error) {
+    console.error('Error al actualizar proyecto',error)
   }
 };
 
 module.exports = {
-    crearProyecto
+  crearProyecto,
+  getProyectos,
+  updateProyecto
 };
