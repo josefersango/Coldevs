1a1815b4d8b368129e7718c1133b4e584e31cc52
Jose Fernando Sanchez Gomez <josefersango@gmail.com>
2021-12-17 00:49:36 -0500

    apoyo integracion back front y creo listas proyectos e inscripciones

--- frontend/gestion-proyectos/src/App.js
+++ frontend/gestion-proyectos/src/App.js
@@ -7,6 +7,11 @@ import IndexUsuario from './pages/usuarios/usuarios'
 import Layout from './components/Layout'
 import EditarUsuario from './pages/usuarios/EditarUsuario'
 import UsuariosPendientes from './pages/usuarios/UsuariosPendientes'
+import EditarProyecto from './pages/proyectos/EditarProyectos'
+import IndexProyecto from './pages/proyectos/ListarProyectos'
+import IndexInscripcion from './pages/inscripciones/ListarInscripciones'
+
+
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000/graphql',
@@ -26,6 +31,9 @@ function App() {
           <Route exact path="/usuarios" element={<IndexUsuario/>}></Route>
           <Route exact path="/usuarios/pendientes" element={<UsuariosPendientes/>}></Route>
           <Route exact path="/usuarios/editar/:_id" element={<EditarUsuario/>}></Route>
+          <Route exact path="/proyectos" element={<IndexProyecto/>}></Route>          
+          <Route exact path="/proyectos/editar/:_id" element={<EditarProyecto/>}></Route>
+          <Route exact path="/inscripcion" element={<IndexInscripcion/>}></Route>
         </Routes>
       </Layout>
       </BrowserRouter>
